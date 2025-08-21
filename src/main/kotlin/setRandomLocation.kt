11c770e936583f387be91907fd8e55f874e35ebc
Bennyz7834 <benmitch46@gmail.com>
2025-08-22 08:40:26 +1000

    Finished setRandomLocation.kt

--- src/main/kotlin/setRandomLocation.kt
+++ src/main/kotlin/setRandomLocation.kt
@@ -1,11 +1,15 @@
 package io.github.loadeddock
 
 import io.github.dockyardmc.location.Location
+import kotlin.random.Random
 
 fun setRandomLocation(location1: Location, location2: Location): Location {
 
-    val randomLocation = location1
-    return location1 //todo
+    if (location1.world != location2.world) {
+        error("location1 & location2 world does not match.")
+    }
+
+    return Location(Random.nextDouble(location1.x, location2.x), Random.nextDouble(location1.y, location2.y), Random.nextDouble(location1.z, location2.z), location1.world)
 
 }
 
