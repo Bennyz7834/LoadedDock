253fb1ff12e30bfb425e2929255f0e6226346275
Bennyz7834 <benmitch46@gmail.com>
2025-08-22 08:00:06 +1000

    Added file for setRandomLocation.kt

---
+++ src/main/kotlin/setRandomLocation.kt
@@ -0,0 +1,11 @@
+package io.github.loadeddock
+
+import io.github.dockyardmc.location.Location
+
+fun setRandomLocation(location1: Location, location2: Location): Location {
+
+    val randomLocation = location1
+    return location1 //todo
+
+}
+
