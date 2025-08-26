89af2770c7ab6f42a38c27a266724115d003ea33
Bennyz7834 <benmitch46@gmail.com>
2025-08-27 07:48:20 +1000

    fixed vector going backwards

--- src/main/kotlin/setVectorBetweenLocations.kt
+++ src/main/kotlin/setVectorBetweenLocations.kt
@@ -5,6 +5,6 @@ import io.github.dockyardmc.maths.vectors.Vector3d
 
 fun setVectorBetweenLocations(startLocation: Location, endLocation: Location): Vector3d {
 
-    return Vector3d(startLocation.x - endLocation.x, startLocation.y - endLocation.y, startLocation.z - endLocation.z)
+    return Vector3d(endLocation.x - startLocation.x, endLocation.y - startLocation.y, endLocation.z - startLocation.z)
 
 }
