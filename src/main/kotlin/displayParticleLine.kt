4dda99c3e26f56a21b9d52b4bb6552bcc75e6402
Bennyz7834 <benmitch46@gmail.com>
2025-08-27 18:40:56 +1000

    more param stuf

--- src/main/kotlin/displayParticleLine.kt
+++ src/main/kotlin/displayParticleLine.kt
@@ -4,10 +4,12 @@ import io.github.dockyardmc.player.Player
 import io.github.dockyardmc.world.World
 import io.github.dockyardmc.location.Location
 import io.github.dockyardmc.maths.vectors.Vector3d
+import io.github.dockyardmc.maths.vectors.Vector3f
+import io.github.dockyardmc.particles.data.ParticleData
 import io.github.dockyardmc.particles.spawnParticle
 import io.github.dockyardmc.registry.registries.Particle
 
-fun Player.displayParticleLine(particle: Particle, startLocation: Location, endLocation: Location, effectSpacing: Double = 0.1) {
+fun Player.displayParticleLine(particle: Particle, startLocation: Location, endLocation: Location, effectSpacing: Double = 0.1, offset: Vector3f = Vector3f(0f, 0f, 0f), speed: Float = 0.5f, amount: Int = 1, alwaysShow: Boolean = false, overrideLimiter: Boolean = false, particleData: ParticleData? = null) {
 
     val vectorBetween = setVectorBetweenLocations(startLocation, endLocation)
     var currentLocation = startLocation.setDirection(vectorBetween)
@@ -20,7 +22,7 @@ fun Player.displayParticleLine(particle: Particle, startLocation: Location, endL
     while (currentDistance <= maxDistance) {
 
         currentLocation = currentLocation.add(stepVec)
-        spawnParticle(particle = particle, location = currentLocation)
+        spawnParticle(particle = particle, location = currentLocation, offset = offset, speed = speed, amount = amount, alwaysShow = alwaysShow, overrideLimiter = overrideLimiter, particleData = particleData)
 
         currentDistance += effectSpacing
 
@@ -29,14 +31,14 @@ fun Player.displayParticleLine(particle: Particle, startLocation: Location, endL
 }
 
 
-fun World.displayParticleLine(particle: Particle, startLocation: Location, endLocation: Location, effectSpacing: Double) {
+fun World.displayParticleLine(particle: Particle, startLocation: Location, endLocation: Location, effectSpacing: Double = 0.1, offset: Vector3f = Vector3f(0f, 0f, 0f), speed: Float = 0.5f, amount: Int = 1, alwaysShow: Boolean = false, overrideLimiter: Boolean = false, particleData: ParticleData? = null) {
 
     this.players.displayParticleLine(particle, startLocation, endLocation, effectSpacing)
 
 }
 
 
-fun Collection<Player>.displayParticleLine(particle: Particle, startLocation: Location, endLocation: Location, effectSpacing: Double) {
+fun Collection<Player>.displayParticleLine(particle: Particle, startLocation: Location, endLocation: Location, effectSpacing: Double = 0.1, offset: Vector3f = Vector3f(0f, 0f, 0f), speed: Float = 0.5f, amount: Int = 1, alwaysShow: Boolean = false, overrideLimiter: Boolean = false, particleData: ParticleData? = null) {
 
     this.forEach {player -> player.displayParticleLine(particle, startLocation, endLocation, effectSpacing)}
 
